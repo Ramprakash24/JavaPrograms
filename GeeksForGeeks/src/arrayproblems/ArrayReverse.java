f119c3ea60d13e93ad4189fd2e5135118761962e
Ramprakash24 <ramprakashd246@gmail.com>
2018-06-20 20:22:27 -0700

    java array

---
+++ GeeksForGeeks/src/arrayproblems/ArrayReverse.java
@@ -0,0 +1,25 @@
+package arrayproblems;
+// Question : https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+public class ArrayReverse {
+	void reverseArray(int[] array, int start, int end) {
+	int temp;
+	if(start >= end)
+		return;
+	temp = array[start];
+	array[start] = array[end];
+	array[end] = temp;
+	reverseArray(array, start+1, end-1);
+	}
+	
+	void printArray(int[] printArr) {
+		for(int i = 0; i < printArr.length; i++)
+			System.out.println(printArr[i]);
+	}
+	
+	public static void main(String args[]) {
+		ArrayReverse reverse = new ArrayReverse();
+		int[] arrayValue = {100,200,300,400,500,600,700};
+		reverse.reverseArray(arrayValue, 0, arrayValue.length-1);
+		reverse.printArray(arrayValue);		
+	}
+}
---
+++ learning/src/doubts/VariableDemo.java
@@ -0,0 +1,20 @@
+package doubts;
+
+public class VariableDemo
+{
+	static int count=0;
+	   public void increment()
+	   {
+	       count++;
+	   }
+	   public static void main(String args[])
+	   {
+	       VariableDemo obj1=new VariableDemo();
+	       VariableDemo obj2=new VariableDemo();
+	       obj1.increment();
+	       obj2.increment();
+	       obj2.increment();
+	       System.out.println("Obj1: count is="+count);
+	       System.out.println("Obj2: count is="+count);
+	   }
+	}
